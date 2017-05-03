51c9f91b07041338926c9a341497fc8c660ea7f3
Mike Farrow <contact@mikefarrow.co.uk>
2017-05-03 01:39:48 +0100

    adding splash screen

--- packages.sh
+++ packages.sh
@@ -10,6 +10,7 @@ sudo apt-get install -y emulationstation vorbis-tools
 sudo apt-get install -y libretro-nestopia libretro-snes9x-next libretro-mupen64plus libretro-mednafen-psx libretro-gambatte libretro-picodrive libretro-prosystem libretro-stella
 
 sudo apt-get install -y --no-install-recommends xorg openbox pulseaudio compton
+sudo apt-get install -y plymouth plymouth-dbg plymouth-theme-ubuntu-logo
 sudo apt-get install -y upstart-sysv xserver-xorg-legacy devilspie2
 
 sudo apt-get install -y bluetooth rfkill wminput xwiimote
