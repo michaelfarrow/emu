3e5c01f97c3186c1fafb06fc75c57c8534563e32
Mike Farrow <contact@mikefarrow.co.uk>
2017-05-03 01:39:20 +0100

    fixing reboot/shutdown

--- users.sh
+++ users.sh
@@ -4,3 +4,8 @@ sudo adduser kiosk
 sudo usermod -a -G audio kiosk
 sudo usermod -a -G video kiosk
 sudo usermod -a -G input kiosk
+
+sudoers="/etc/sudoers"
+cmds="kiosk ALL=(ALL) NOPASSWD:ALL"
+
+[ -z "$(sudo grep "$cmds" "$sudoers")" ] && echo "$cmds" | sudo tee -a "$sudoers"
